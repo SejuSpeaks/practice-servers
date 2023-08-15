a3b5cb4b61f9c4b7dfda857121ab5626fbac9173
SejuSpeaks <80376392+SejuSpeaks@users.noreply.github.com>
2023-08-15 18:43:47 -0400

    finished part1

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/status',(req,res)=>{
+    res.send('The server is alive!');
+})
+
+app.listen(port,()=> console.log(`server is running on port ${port} `));
